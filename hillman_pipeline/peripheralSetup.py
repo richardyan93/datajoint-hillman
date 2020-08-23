9a5eabaa947f2b8ef6c0778f56326a1495868345
richardyan93 <44348497+richardyan93@users.noreply.github.com>
2020-08-23 17:56:10 -0400

    move behavior and stim setup to peripheral.py

---
+++ hillman_pipeline/peripheralSetup.py
@@ -0,0 +1,34 @@
+import datajoint as dj
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import microscopy
+
+schema = dj.schema('hillman_organism')
+
+@schema
+class BehavioralSetup(dj.Manual):
+    definition = """
+    behavior_setup                       : varchar(32)    # unique nickname of a behavior set up
+    ---
+    behavior_setup_date                  : date
+    behavior_setup_description=''        : varchar(1024)
+    """
+
+    class Camera(dj.Part):
+        definition = """
+        -> master
+        camera_id               :  tinyint unsigned
+        ---
+        -> microscopy.Camera
+        -> [nullable] microscopy.Filter
+        camera_description=''   : varchar(1024)
+        """
+
+
+@schema
+class StimSetup(dj.Manual):
+    definition = """
+    stim_setup                  : varchar(32)     # unique nickname of a stim set up
+    ---
+    stim_description            : varchar(1024)
+    """
