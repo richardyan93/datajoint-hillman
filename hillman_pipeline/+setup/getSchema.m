adbda8b7b777cb44988c7adb83bf5cc818ac9e3c
Richard <ywwrichard@gmail.com>
2020-09-09 18:53:23 -0400

    rename peripheralsetup in matlab to setup

--- hillman_pipeline/+setup/getSchema.m
+++ hillman_pipeline/+setup/getSchema.m
@@ -1,7 +1,7 @@
 function obj = getSchema
 persistent schemaObject
 if isempty(schemaObject)
-    schemaObject = dj.Schema(dj.conn, 'peripheralsetup', 'hillman_peripheralsetup');
+    schemaObject = dj.Schema(dj.conn, 'setup', 'hillman_setup');
 end
 obj = schemaObject;
 end
